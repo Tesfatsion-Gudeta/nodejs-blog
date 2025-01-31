4bda1f18e0a042ba384688ef478b0e8912487777
tesfatsion <tesfatsiongudeta7@gmail.com>
2025-01-31 02:35:50 -0800

    add views and layout for Node.js blog application; implement home and about pages

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 //routes
 router.get("/", (req, res) => {
-  res.send("hello world!");
+
+  const locals={
+    title:"NodeJs Blog",
+    description:"simple blog created with nodejs,express and mongodb"
+  }
+  res.render("index",locals)
+});
+
+router.get("/about", (req, res) => {
+  res.render("about")
 });
 
 module.exports = router;
